049d820eec33ed458fc8041240452998b0c8d0e8
Martin Weinelt <hexa@darmstadt.ccc.de>
2018-09-14 22:37:15 +0200

    fix ci scripts

--- ci_build.sh
+++ ci_build.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
-$(eval TMPDIR := $(shell mktemp -d))
-pacstrap -C /usr/share/devtools/pacman-multilib.conf -c -d -G -M $(TMPDIR) $(shell cat packages)
-cp --recursive --preserve=timestamps --backup --suffix=.pacnew rootfs/* $(TMPDIR)/
-cp /usr/share/devtools/pacman-multilib.conf $(TMPDIR)/etc/pacman.conf
+#!/bin/sh -ex
 
-arch-chroot $(TMPDIR) locale-gen
-arch-chroot $(TMPDIR) pacman-key --init
-arch-chroot $(TMPDIR) pacman-key --populate archlinux
+export TMPDIR=$(mktemp -d)
+pacstrap -C /usr/share/devtools/pacman-multilib.conf -c -d -G -M $TMPDIR $(cat packages)
+cp --recursive --preserve=timestamps --backup --suffix=.pacnew rootfs/* $TMPDIR/
+cp /usr/share/devtools/pacman-multilib.conf $TMPDIR/etc/pacman.conf
 
-tar --numeric-owner --xattrs --acls --exclude-from=exclude -C $(TMPDIR) -c . -f archlinux.tar
-rm -rf $(TMPDIR)
-docker build -t $(DOCKER_ORGANIZATION)/$(DOCKER_IMAGE) .
+arch-chroot $TMPDIR locale-gen
+arch-chroot $TMPDIR pacman-key --init
+arch-chroot $TMPDIR pacman-key --populate archlinux
+
+tar --numeric-owner --xattrs --acls --exclude-from=exclude -C $TMPDIR -c . -f archlinux.tar
+rm -rf $TMPDIR
+docker build -t $DOCKER_ORGANIZATION/$DOCKER_IMAGE .
