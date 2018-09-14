e49958a8c475ddaa28e4e23d2d20e2410f8e82c6
Martin Weinelt <hexa@darmstadt.ccc.de>
2018-09-14 22:41:33 +0200

    split build into prepare and build

--- ci_build.sh
+++ ci_build.sh
@@ -1,14 +1,3 @@
 #!/bin/sh -ex
 
-export TMPDIR=$(mktemp -d)
-pacstrap -C /usr/share/devtools/pacman-multilib.conf -c -d -G -M $TMPDIR $(cat packages)
-cp --recursive --preserve=timestamps --backup --suffix=.pacnew rootfs/* $TMPDIR/
-cp /usr/share/devtools/pacman-multilib.conf $TMPDIR/etc/pacman.conf
-
-arch-chroot $TMPDIR locale-gen
-arch-chroot $TMPDIR pacman-key --init
-arch-chroot $TMPDIR pacman-key --populate archlinux
-
-tar --numeric-owner --xattrs --acls --exclude-from=exclude -C $TMPDIR -c . -f archlinux.tar
-rm -rf $TMPDIR
 docker build -t $DOCKER_ORGANIZATION/$DOCKER_IMAGE .
